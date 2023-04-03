e3e727762b8a2c8e8801d839bcf0e2d3f4286c75
Lyna-NA <67642540+Lyna-NA@users.noreply.github.com>
2023-04-03 09:11:47 +0300

    add prediction for address

--- public/js/add.js
+++ public/js/add.js
@@ -1,6 +1,7 @@
 const storeForm = document.getElementById('store-form');
 const storeId = document.getElementById('store-id');
 const storeAddress = document.getElementById('store-address');
+c
 
 // Send POST to API to add store
 async function addStore(e) {
